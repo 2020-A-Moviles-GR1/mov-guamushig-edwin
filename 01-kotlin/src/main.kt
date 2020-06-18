9bf629b57f53dc9d2b933fe0fb6afec6c38ef37d
Edwin Guamushig <edwin.paul.73@gmail.com>
2020-06-18 08:11:33 -0500

    termino operadores

--- 01-kotlin/src/main.kt
+++ 01-kotlin/src/main.kt
@@ -90,8 +90,7 @@ fun arreglos() {
 //        println("Valor de iteracion ${valor}")
     }
 
-    val modificado = arregloNumeros.map {
-        valor ->
+    val modificado = arregloNumeros.map { valor ->
         val nuevo = valor * 5
         return@map nuevo
     }
@@ -100,24 +99,84 @@ fun arreglos() {
     val negataivos = arregloNumeros.map { it * -1 }
     println(negataivos)
 
-    val filtrado = arregloNumeros.filter {
-        valor ->
+    val filtrado = arregloNumeros.filter { valor ->
         val nuevo = valor > 2
         return@filter nuevo
     }
     println(filtrado)
 
-    val reducido = arregloNumeros.reduce{
-        acumulador, elemento ->
+    val reducido = arregloNumeros.reduce { acumulador, elemento ->
         acumulador + elemento
     }
     println(reducido)
+
+    val fold = arregloNumeros.fold(3, { acumulador, elemento ->
+        acumulador + elemento
+    })
+    println(fold)
 
     arrayVariable.addAll(listOf("Pedro", "Maria", "Paquita"));
     val indice = arrayVariable.indexOf("Maria");
     arrayVariable[indice] = "Camilitaa";
     arrayVariable.removeAt(indice)
-    val numbers = listOf(1, 2, 3)
+    val numbers = listOf(7, 4, 9, 6, 3, 1, 0)
     val res = numbers.map { it * it }
+    val ordenado = numbers.sorted()
+    val encontre = numbers.find { it == 14 }
+
+    val encontreTF = numbers.any { it ->
+        val menor = it > 5
+        return@any menor
+    }
+    println(encontreTF)
+
+    val encontreT = numbers.all { it ->
+        val menor = it > 5
+        return@all menor
+    }
+    println(encontreT)
+
+    println(ordenado)
+    println(encontre)
 }
 
+fun bucles() {
+    var myArray: List<Int> = arrayListOf(1, 2, 3, 4, 5, 6);
+    var myMap: MutableMap<String, Int> = mutableMapOf("Juan" to 2, "Emilio" to 56)
+    println(myArray)
+    println(myMap)
+
+    for (myString in myArray) {
+        println(myString)
+    }
+
+    for (element in myMap) {
+        println("${element.key} es la llave y ${element.value} es el valor")
+    }
+
+    for (x in 0 until 5) {
+        print("${x} - ")
+    }
+
+    for (x in 0 until 10 step 2) {
+        print("${x} - ")
+    }
+    println("\n")
+    for (x in 10 downTo 0) {
+        print("${x} - ")
+    }
+}
+
+fun valoresNull() {
+    var nulo: String? = "cosaaa";
+    println(nulo?.length)
+    nulo = null;
+    println(nulo?.length) // !! obliga que no sea nula
+
+    var stringsito: String? = "Este es un mesanje";
+    stringsito?.let {
+        println(it)
+    } ?: run {
+        println("Fueraa")
+    }
+}
